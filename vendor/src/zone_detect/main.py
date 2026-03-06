44556cc79b27709231ef780a7acf6de85aa378aa
KelianPons <kelian.pons@centrale.centralelille.fr>
2026-03-06 14:18:30 +0100

    feat: adding flair code

---
+++ vendor/src/zone_detect/main.py
@@ -0,0 +1,262 @@
+import os, sys
+import datetime
+import warnings
+import torch
+import rasterio
+import yaml
+
+from pathlib import Path
+import argparse 
+from torch.utils.data import DataLoader
+from pytorch_lightning.utilities.rank_zero import rank_zero_only  
+from rasterio.features import geometry_window
+from tqdm import tqdm
+
+from src.zone_detect.slicing_job import slice_extent, create_polygon_from_bounds
+from src.zone_detect.model import load_model
+from src.zone_detect.dataset import Sliced_Dataset, convert
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+#### CONF FILE
+argParser = argparse.ArgumentParser()
+argParser.add_argument("--conf", help="Path to the .yaml config file")
+
+
+@rank_zero_only
+class Logger(object):
+    def __init__(self, filename='Default.log'):
+        self.terminal = sys.stdout
+        self.log = open(filename, 'w', encoding='utf-8') 
+        self.encoding = self.terminal.encoding
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        self.log.flush()
+
+
+def read_config(file_path):
+    with open(file_path, "r") as f:
+        return yaml.safe_load(f)
+
+def _format_hms(td: datetime.timedelta) -> str:
+    total_seconds = int(td.total_seconds())
+    h = total_seconds // 3600
+    m = (total_seconds % 3600) // 60
+    s = total_seconds % 60
+    return f"{h:02d}:{m:02d}:{s:02d}"
+
+
+def setup(args):
+    config = read_config(args.conf)
+    use_gpu = (False if torch.cuda.is_available() is False else config['use_gpu'])
+    device = torch.device("cuda" if use_gpu else "cpu")
+
+    assert isinstance(config['output_path'], str), "Output path does not exist."
+    assert os.path.exists(config['input_img_path']), "Input image path does not exist."
+    assert config['margin'] * 2 < config['img_pixels_detection'], "Margin is too large : margin*2 < img_pixels_detection"
+    assert config['output_type'] in ['class_prob', 'argmax'], "Invalid output type: should be argmax or class_prob."
+    assert config['norma_task'][0]['norm_type'] in ['custom', 'scaling'], "Invalid normalization type: should be custom or scaling."
+    assert os.path.isfile(config['model_weights']), "Model weights file does not exist."
+    if not config['output_name'].endswith('.tif'):
+        config['output_name'] += '.tif'  
+
+    try:
+        Path(config['output_path']).mkdir(parents=True, exist_ok=True)
+        base_name = config['output_name']
+        path_out = os.path.join(config['output_path'], base_name)
+
+        # If it's a .tif file, handle that
+        filename, ext = os.path.splitext(base_name)
+        counter = 1
+
+        while os.path.exists(path_out):
+            new_name = f"{filename}_{counter}{ext}"
+            path_out = os.path.join(config['output_path'], new_name)
+            counter += 1
+        config['output_name'] = os.path.splitext(os.path.basename(path_out))[0]
+        return config, path_out, device, use_gpu
+
+    except Exception as error:
+        print(f"Something went wrong during detection configuration: {error}")
+
+
+
+
+
+def conf_log(config, resolution, img_size):
+    # Determine model template info based on provider
+    provider = config['model_framework']['model_provider']
+    if provider == 'HuggingFace':
+        model_template = f"{provider} - {config['model_framework']['HuggingFace']['org_model']}"
+    elif provider == 'SegmentationModelsPytorch':
+        model_template = f"{provider} - {config['model_framework']['SegmentationModelsPytorch']['encoder_decoder']}"
+    else:
+        model_template = provider  # fallback if unknown
+
+    print(f"""
+    |- output path: {config['output_path']}
+    |- output raster name: {config['output_name']}
+
+    |- input image path: {config['input_img_path']}
+    |- channels: {config['channels']}
+    |- input image WxH: {img_size[0], img_size[1]}   
+    |- resolution: {resolution}
+    |- image size for detection: {config['img_pixels_detection']}
+    |- overlap margin: {config['margin']}
+    |- write dataframe: {config['write_dataframe']}
+    |- number of classes: {config['n_classes']}
+    |- normalization: {config['norma_task'][0]['norm_type']}
+    |- output type: {config['output_type']}
+
+    |- model weights path: {config['model_weights']}
+    |- model template: {model_template}
+    |- device: {"cuda" if config['use_gpu'] else "cpu"}
+    |- batch size: {config['batch_size']}\n
+    """)
+      
+
+
+
+def prepare(config, device):
+    
+    print(f"""
+    ##############################################
+    ZONE DETECTION
+    ##############################################
+
+    CUDA available? {torch.cuda.is_available()}""")
+
+    ## slicing extent for overlapping detection 
+    sliced_dataframe, profile, resolution, img_size = slice_extent(in_img=config['input_img_path'],
+                                                                patch_size=config['img_pixels_detection'],  
+                                                                margin=config['margin'], 
+                                                                output_name=config['output_name'],
+                                                                output_path=config['output_path'],
+                                                                write_dataframe=config['write_dataframe'],
+                                                               )
+    ## log
+    conf_log(config, resolution, img_size)
+    print(f"""    [x] sliced input raster to {len(sliced_dataframe)} squares...""")
+    ## loading model and weights
+    model = load_model(config)
+    model.eval()
+    model = model.to(device)  
+    print(f"""    [x] loaded model and weights...""")
+
+    return sliced_dataframe, profile, resolution, model
+    
+
+
+
+def main():
+
+    start_dt = datetime.datetime.now()
+    print(f"    [STARTING TIME] {start_dt.strftime('%Y-%m-%d %H:%M:%S')}")
+    
+    # reading yaml
+    args = argParser.parse_args()
+    config, path_out, device, use_gpu = setup(args)
+
+    log_filename = os.path.join(config['output_path'], f"{config['output_name']}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
+    sys.stdout = Logger(filename=log_filename)
+    sys.stderr = sys.stdout
+    print(f"    [LOGGER] Writing logs to: {log_filename}")
+
+
+
+    input_img_path = config['input_img_path']
+    channels = config['channels']
+    img_pixels_detection = config['img_pixels_detection']
+    norma_task = config['norma_task']
+    batch_size = config['batch_size']    
+    num_worker = config['num_worker']
+    output_type = config['output_type']    
+    margin = config['margin']
+    n_classes = config['n_classes']
+
+    # slicing and model gathering
+    sliced_dataframe, profile, resolution, model = prepare(config, device)
+    
+    # get dataset 
+    dataset = Sliced_Dataset(dataframe=sliced_dataframe,
+                            img_path=input_img_path,
+                            resolution=resolution,
+                            bands=channels, 
+                            patch_detection_size=img_pixels_detection,
+                            norma_dict=norma_task,
+                            )    
+    
+    # prepare output raster
+    out_overall_profile = profile.copy()
+    out_overall_profile.update({'dtype':'uint8', 'compress':'LZW', 'driver':'GTiff', 'BIGTIFF':'YES', 'tiled':True, 
+                                'blockxsize':img_pixels_detection, 'blockysize':img_pixels_detection})
+    out_overall_profile['count'] = [1 if output_type == 'argmax' else n_classes][0]
+    out_overall_profile.pop('photometric', None)
+    out = rasterio.open(path_out, 'w+', **out_overall_profile)   
+    
+    # get Dataloader
+    data_loader = DataLoader(dataset,
+                             batch_size=batch_size,
+                             num_workers=num_worker,
+                             pin_memory=True,
+                             )
+    # inference loop
+    print(f"""    [ ] starting inference...\n""")
+    for samples in tqdm(data_loader):
+        imgs = samples["image"]
+        imgs = imgs.to(device, non_blocking=(device.type == "cuda"))
+        if use_gpu:
+            torch.cuda.synchronize()
+        with torch.no_grad():
+            logits = model(imgs)
+            if config['model_framework']['model_provider'] == 'HuggingFace':
+                logits = logits.logits
+            logits.to(device)
+        predictions = torch.softmax(logits, dim=1)
+        predictions = predictions.cpu().numpy()
+        indices = samples["index"].cpu().numpy()    
+
+        # writing windowed raster to output rastert 
+        for prediction, index in zip(predictions, indices):
+            # removing margins
+            prediction = prediction[:,0+margin:img_pixels_detection-margin,0+margin:img_pixels_detection-margin]
+            prediction = convert(prediction, output_type)
+            sliced_patch_bounds = create_polygon_from_bounds(sliced_dataframe.at[index[0], 'left'], sliced_dataframe.at[index[0], 'right'], 
+                                                             sliced_dataframe.at[index[0], 'bottom'], sliced_dataframe.at[index[0], 'top'])
+            window = geometry_window(out, [sliced_patch_bounds], pixel_precision=6)
+            window = window.round_shape(op='ceil', pixel_precision=4)
+            #write
+            if output_type == "argmax":
+                out.write(prediction, window=window)
+            else:
+                out.write_band([i for i in range(1, n_classes + 1)], prediction, window=window)      
+                
+    out.close()
+    dataset.close_raster()
+    print(f"""    
+                        
+    [X] done writing to {path_out.split('/')[-1]} raster file.\n""")
+
+    end_dt = datetime.datetime.now()
+    total = end_dt - start_dt
+    print(f"    [ENDING TIME]   {end_dt.strftime('%Y-%m-%d %H:%M:%S')}")
+    print(f"    [TOTAL PROCESSING TIME] { _format_hms(total) }")
+    
+    sys.stdout = sys.__stdout__ 
+
+if __name__ == '__main__':
+    main()         
+
+
+
+
+
+    
+
+
+
