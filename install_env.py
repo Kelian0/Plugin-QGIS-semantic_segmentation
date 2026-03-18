c1c4ac4cf8d1619392cd72c8bd2972c5ea71f5cd
Kélian PONS <kelian.pons@gmail.com>
2026-03-18 13:17:48 +0100

    Fix: update install for windows

--- install_env.py
+++ install_env.py
@@ -2,20 +2,27 @@ import os
 import subprocess
 import platform
 
-def run_command_with_log(command, log_callback):
+def run_command_with_log(command, log_callback, log_file_path=None, custom_env=None):
     process = subprocess.Popen(
         command,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
-        bufsize=1
+        bufsize=1,
+        env=custom_env
     )
     
+    if log_file_path != None:
+        log_file = open(log_file_path, "a", encoding="utf-8")
+        
     for line in iter(process.stdout.readline, ''):
         clean_line = line.strip()
         if clean_line != "":
             if log_callback != None:
                 log_callback(clean_line)
+            if log_file_path != None:
+                log_file.write(clean_line + "\n")
+                log_file.flush()
                 
     process.wait()
     
@@ -31,7 +38,7 @@ def get_micromamba(plugin_dir):
     exe_path = ""
     
     if system == "windows":
-        exe_path = os.path.join(mamba_base_dir, "Windows", "micromamba.exe")
+        exe_path = os.path.join(mamba_base_dir, "Windows","Library","bin", "micromamba.exe")
         
     if system == "darwin":
         if arch == "arm64":
@@ -50,11 +57,24 @@ def get_micromamba(plugin_dir):
 def setup_flair_environment(plugin_dir,mamba_exe, env_dir,log_callback=None,progress_callback=None):
 
     req_file = os.path.join(plugin_dir, "vendor", "FLAIR-1", "flair.egg-info", "requires.txt")
+    log_path = os.path.join(plugin_dir, "install_log.txt")
+
+    clean_env = os.environ.copy()
+        
+    if "PYTHONPATH" in clean_env:
+        del clean_env["PYTHONPATH"]
+        
+    if "PYTHONHOME" in clean_env:
+        del clean_env["PYTHONHOME"]
     
     dependencies = []
-    if progress_callback != None:
-            progress_callback(10)
     
+    if progress_callback != None:
+        progress_callback(10)
+    
+    if os.path.exists(log_path):
+        os.remove(log_path)
+            
     if os.path.exists(req_file):
         with open(req_file, 'r') as file:
             for line in file:
@@ -62,25 +82,43 @@ def setup_flair_environment(plugin_dir,mamba_exe, env_dir,log_callback=None,prog
                 if clean_line != "":
                     dependencies.append(clean_line)
                     
-    if len(dependencies) > 0:
-        base_cmd = [
-            mamba_exe, "create", "-y", "-p", env_dir, 
-            "-c", "conda-forge", "python=3.10", "pip", "gdal"
-        ]
-        run_command_with_log(base_cmd, log_callback)
+    if len(dependencies) == 0:
+        return False
 
-        if progress_callback != None:
-            progress_callback(50)
+    base_cmd = [
+        mamba_exe, "create", "-y", "-p", env_dir, 
+        "-c", "conda-forge", "--override-channels", 
+        "python=3.10", "pip", "gdal"
+    ]
+    
+    mamba_ok = run_command_with_log(base_cmd, log_callback, log_path, clean_env)
+    
+    if mamba_ok == False:
+        if log_callback != None:
+            log_callback("Error: Failed to create Conda environment. Check install_log.txt")
+        return False
+
+    if progress_callback != None:
+        progress_callback(50)
+    
+    python_exe = os.path.join(env_dir, "bin", "python")
+    
+    if os.name == "nt":
+        python_exe = os.path.join(env_dir, "python.exe")
         
-        pip_exe = os.path.join(env_dir, "bin", "pip")
+    pip_cmd = [python_exe, "-m", "pip", "install"] + dependencies
+    
+    pip_ok = run_command_with_log(pip_cmd, log_callback, log_path, clean_env)
+    
+    if pip_ok == False:
+        if log_callback != None:
+            log_callback("Error: Failed to install pip dependencies. Check install_log.txt")
+        return False
         
-        if os.name == "nt": #windows
-            pip_exe = os.path.join(env_dir, "Scripts", "pip.exe")
-            
-        pip_cmd = [pip_exe, "install"] + dependencies
-        run_command_with_log(pip_cmd, log_callback)
-        if progress_callback != None:
-            progress_callback(100)
+    if progress_callback != None:
+        progress_callback(100)
+        
+    return True
 
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
